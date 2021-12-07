61f155bf3a2af4795476dc5535bc6e4f95a7d00f
Your Name <somasolti@gmail.com>
2021-12-07 14:24:37 +0100

    BillItem.vue és Bill.vue hozzáadása

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Bill  />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Bill from './components/Bill.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Bill
   },
   data() {
     return {
---
+++ src/components/Bill.vue
@@ -0,0 +1,18 @@
+<template>
+  <table>
+      <th>Név</th>
+      <th>Ár</th>
+      <th>Darabszám</th>
+
+    </table>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
